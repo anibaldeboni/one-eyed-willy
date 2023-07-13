d21a4dcf7b6c68b2903f0dbbd005dc264f11a161
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2023-07-13 09:54:34 -0300

    add merge pdf bad request test

--- internal/handler/pdf_test.go
+++ internal/handler/pdf_test.go
@@ -15,58 +15,81 @@ import (
 )
 
 func TestGeneratePdfCaseSuccess(t *testing.T) {
-	setup()
 	var (
 		reqJSON = `{"html":"PGh0bWw+CjxoZWFkPgoJPHRpdGxlPk15IFBERiBGaWxlPC90aXRsZT4KPC9oZWFkPgo8Ym9keT4KCTxwPkhlbGxvIHRoZXJlISBJJ20gYSBwZGYgZmlsZSBnZW5lcmF0ZSBmcm9tIGEgaHRtbCB1c2luZyBnbyBhbmQgZ29wZGYgcGFja2FnZTwvcD4KPC9ib2R5Pgo8L2h0bWw+"}`
 	)
-	req := httptest.NewRequest(echo.POST, "/pdf", strings.NewReader(reqJSON))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	assert.NoError(t, h.GeneratePdfFromHTML(c))
+	ctx, rec := setupServer(
+		httptest.NewRequest(echo.POST, "/pdf", strings.NewReader(reqJSON)),
+		echo.MIMEApplicationJSON,
+	)
+
+	assert.NoError(t, h.GeneratePdfFromHTML(ctx))
 	if assert.Equal(t, http.StatusOK, rec.Code) {
-		assert.Equal(t, "application/pdf", rec.Header().Clone().Get("Content-Type"))
+		assert.Equal(t, MIMEApplicationPdf, rec.Header().Clone().Get("Content-Type"))
 		assert.Greater(t, len(rec.Body.Bytes()), 0)
 		assert.NotEmpty(t, rec.Body)
 	}
 }
 
-func TestMergePdfsCaseSuccess(t *testing.T) {
-	setup()
+func TestMergePdfs(t *testing.T) {
+	tests := []struct {
+		name          string
+		numberOfFiles int
+		httpStatus    int
+	}{
+		{
+			name:          "when two files are provided",
+			numberOfFiles: 2,
+			httpStatus:    http.StatusOK,
+		},
+		{
+			name:          "when just one file is provided",
+			numberOfFiles: 1,
+			httpStatus:    http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, boundary := createBody(t, tt.numberOfFiles)
 
-	body := new(bytes.Buffer)
-	writer := multipart.NewWriter(body)
+			ctx, rec := setupServer(
+				httptest.NewRequest(echo.POST, "/pdf/merge", body),
+				fmt.Sprintf("%s; boundary=%s", echo.MIMEMultipartForm, boundary),
+			)
 
-	file1, file2 := loadFiles(t)
-	part, _ := writer.CreateFormFile("files", "file1.pdf")
-	part.Write(file1)
-
-	part2, _ := writer.CreateFormFile("files", "file2.pdf")
-	part2.Write(file2)
-	writer.Close()
-
-	req := httptest.NewRequest(echo.POST, "/pdf/merge", body)
-	contentType := fmt.Sprintf("%s; boundary=%s", echo.MIMEMultipartForm, writer.Boundary())
-	req.Header.Add(echo.HeaderContentType, contentType)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-
-	if assert.NotPanics(t, func() { _ = h.MergePdfs(c) }) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.NoError(t, h.MergePdfs(c))
+			if assert.NotPanics(t, func() { _ = h.MergePdfs(ctx) }) {
+				assert.Equal(t, tt.httpStatus, rec.Code)
+				assert.NoError(t, h.MergePdfs(ctx))
+			}
+		})
 	}
 }
 
-func loadFiles(t *testing.T) (file1 []byte, file2 []byte) {
-	file1, err := os.ReadFile("../../testdata/file1.pdf")
-	if err != nil {
-		t.Fatal("testdata/file1.pdf not found")
-	}
+func createBody(t *testing.T, numberOfFiles int) (*bytes.Buffer, string) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
 
-	file2, err = os.ReadFile("../../testdata/file2.pdf")
-	if err != nil {
-		t.Fatal("testdata/file2.pdf not found")
+	for i := 0; i < numberOfFiles; i++ {
+		file, err := os.ReadFile("../../testdata/file1.pdf")
+		if err != nil {
+			t.Fatal("testdata/file1.pdf not found")
+		}
+		part, _ := writer.CreateFormFile("files", fmt.Sprintf("file%d.pdf", i))
+		part.Write(file)
 	}
+	writer.Close()
 
-	return file1, file2
+	return body, writer.Boundary()
+}
+
+func setupServer(
+	req *http.Request,
+	contentType string,
+) (echo.Context, *httptest.ResponseRecorder) {
+	setup()
+	req.Header.Set(echo.HeaderContentType, contentType)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	return c, rec
 }
