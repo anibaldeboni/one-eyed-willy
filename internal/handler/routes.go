47b062b9de3aa1109b7aedf4cb29ad762b9cdbbd
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2023-08-15 12:24:38 -0300

    add static import

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -1,12 +1,25 @@
 package handler
 
 import (
+	"embed"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
+//go:embed static/*
+var static embed.FS
+
 func (h *Handler) addRoutes(e *echo.Echo) {
+	staticHandler := echo.WrapHandler(http.FileServer(http.FS(static)))
+	// The embedded files will all be in the '/static' folder so need to rewrite the request (could also do this with fs.Sub)
+	staticRewrite := middleware.Rewrite(map[string]string{"/*": "/static/$1"})
+
 	e.Renderer = h.NewTemplateRegistry()
+
+	e.GET("/*", staticHandler, staticRewrite)
 	e.GET("/", h.HomeView)
 	e.GET("/docs/*", echoSwagger.WrapHandler)
 	e.GET("/generate", h.CreatePdfFromHtmlView)
