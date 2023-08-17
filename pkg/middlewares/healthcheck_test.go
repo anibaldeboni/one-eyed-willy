6cd583f0b6f520969248f3fa0b8ab2a76a06415c
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2023-08-17 10:04:05 -0300

    improve covereage

--- pkg/middlewares/healthcheck_test.go
+++ pkg/middlewares/healthcheck_test.go
@@ -10,19 +10,71 @@ import (
 )
 
 func TestHealthCheck(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, healthCheckPath, nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
 	handler := func(c echo.Context) error {
 		return c.String(http.StatusOK, "test")
 	}
+	tests := []struct {
+		name        string
+		path        string
+		response    []byte
+		contentType string
+	}{
+		{
+			name:        "when the path is /health",
+			path:        healthCheckPath,
+			response:    []byte{0x7b, 0x22, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22, 0x3a, 0x22, 0x4f, 0x4b, 0x22, 0x7d, 0xa}, // {"status":"OK"},
+			contentType: echo.MIMEApplicationJSONCharsetUTF8,
+		},
+		{
+			name:        "when the path is other than /health",
+			path:        "/other-path",
+			response:    []byte("test"),
+			contentType: echo.MIMETextPlainCharsetUTF8,
+		},
+	}
+	for _, tt := range tests {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, tt.path, nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
 
-	body := []byte{0x7b, 0x22, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22, 0x3a, 0x22, 0x4f, 0x4b, 0x22, 0x7d, 0xa} // {"status":"OK"}
+		if assert.NoError(t, HealthCheck()(handler)(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, tt.contentType, rec.Header().Get(echo.HeaderContentType))
+			assert.Equal(t, tt.response, rec.Body.Bytes())
+		}
+	}
+}
 
-	if assert.NoError(t, HealthCheck()(handler)(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, echo.MIMEApplicationJSONCharsetUTF8, rec.Header().Get(echo.HeaderContentType))
-		assert.Equal(t, body, rec.Body.Bytes())
+func TestHealthCheckWithConfig(t *testing.T) {
+	handler := func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	tests := []struct {
+		name string
+		path string
+	}{
+		{
+			name: "when the path is /health",
+			path: "/health",
+		},
+		{
+			name: "when the path is other than /health",
+			path: "/my-health-check",
+		},
+	}
+	for _, tt := range tests {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, tt.path, nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		want := []byte{0x7b, 0x22, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22, 0x3a, 0x22, 0x4f, 0x4b, 0x22, 0x7d, 0xa} // {"status":"OK"},
+
+		if assert.NoError(t, HealthCheckWithConfig(tt.path)(handler)(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, echo.MIMEApplicationJSONCharsetUTF8, rec.Header().Get(echo.HeaderContentType))
+			assert.Equal(t, want, rec.Body.Bytes())
+		}
 	}
 }
