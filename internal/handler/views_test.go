4f0366ab0c65bc06a78b7fbc9705669b3f0723a6
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2023-08-19 16:08:18 -0300

    add template registry tests

--- internal/handler/views_test.go
+++ internal/handler/views_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -38,6 +39,37 @@ func TestViews(t *testing.T) {
 			assert.Equal(t, http.StatusOK, rec.Code)
 			assert.Equal(t, echo.MIMETextHTMLCharsetUTF8, rec.Header().Get(echo.HeaderContentType))
 			assert.NotEmpty(t, rec.Body)
+		})
+	}
+}
+
+func TestTemplateRegistry(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		wantErr bool
+	}{
+		{
+			name:    "when template is valid",
+			path:    "home.html",
+			wantErr: false,
+		},
+		{
+			name:    "when template is invalid",
+			path:    "madonna-standing-in-the-front-yard.html",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h, e := setupTestHandler(nil, nil)
+			tr := h.NewTemplateRegistry()
+			buf := new(bytes.Buffer)
+			err := tr.Render(buf, tt.path, nil, e.NewContext(nil, nil))
+
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.Equal(t, tt.wantErr, buf.Len() == 0)
 		})
 	}
 }
