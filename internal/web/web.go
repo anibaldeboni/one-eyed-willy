cf36c5efcf5e6da902c14cae80c1acb423708f21
Aníbal Deboni Neto <anibal.deboni@creditas.com>
2023-08-19 18:19:22 -0300

    fix logger config

--- internal/web/web.go
+++ internal/web/web.go
@@ -16,24 +16,26 @@ func New(logger *zap.SugaredLogger) *echo.Echo {
 
 	e := echo.New()
 	e.Binder = NewFileBinder(e.Binder)
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Recover())
+	e.Use(middlewares.HealthCheck())
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogURI:      true,
-		LogStatus:   true,
-		LogError:    true,
-		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogURI:       true,
+		LogStatus:    true,
+		LogError:     true,
+		LogLatency:   true,
+		LogMethod:    true,
+		LogRequestID: true,
+		HandleError:  true,
 		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
 			if v.Error == nil {
-				logger.Infof("method=%s path=%s status=%d request_id=%s", v.Method, v.URI, v.Status, v.RequestID)
+				logger.Infof("method=%s path=%s status=%d request_id=%s latency=%d", v.Method, v.URI, v.Status, v.RequestID, v.Latency)
 			} else {
-				logger.Errorf("method=%s path=%s status=%d request_id=%s error=%v", v.Method, v.URI, v.Status, v.RequestID, v.Error)
+				logger.Errorf("method=%s path=%s status=%d request_id=%s latency=%d error=%v", v.Method, v.URI, v.Status, v.RequestID, v.Latency, v.Error)
 			}
 			return nil
 		},
 	}))
-	// e.Use(middleware.LoggerWithConfig(config.GetEchoLogConfig()))
-	e.Use(middleware.Recover())
-	e.Use(middleware.RequestID())
-	e.Use(middlewares.HealthCheck())
 	e.HideBanner = true
 	e.Validator = NewValidator()
 
